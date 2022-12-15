2907ed4ab3ee248dceb613da34f4f2d7f8a35dbe
Pratham <prathamsrivastavas@gmail.com>
2022-12-15 19:19:25 +0530

    new codes added

---
+++ Basic/JavBasics.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class JavBasics {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // String input=sc.next();
+        // System.out.println(input);
+       
+
+        // String name=sc.nextLine();
+        // System.out.println(name);
+
+
+        boolean bool=sc.nextBoolean();
+        System.out.println(bool);
+
+        sc.close();
+        
+    }
+}
---
+++ Basic/ProductOfTwoNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ProductOfTwoNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        int radi=sc.nextInt();
+        // double pi=3.14;
+        float pi=3.14f;
+        float area=radi*radi*pi;
+        System.out.println(area);
+        sc.close();
+    }
+}
